8e20099907cc38715077792e439eddceba6ff427
qwertys50 <kchess5gorsk2@gmail.com>
2026-02-16 14:50:39 +0300

    Update tdut_origin.lua

--- tdut_origin.lua
+++ tdut_origin.lua
@@ -36,7 +36,6 @@ local function is_file(file_path)
 end
 
 local function save_autofarms()
-        print(HttpService:JSONEncode(my_proz_1))
 	writefile(file_path, game:GetService("HttpService"):JSONEncode({Autofarm = autofarms, my_proz_1=my_proz_1}))
 end
 
@@ -54,7 +53,6 @@ else
 		autofarms = data.Autofarm
 		my_proz_1 = data.my_proz_1
 
-        print(HttpService:JSONEncode(data))
 	end
 end
 
@@ -67,7 +65,7 @@ btn_button_new.BorderSizePixel = 0
 btn_button_new.Image = "rbxassetid://94540935178190"
 
 
-local UI = loadstring(game:HttpGet('https://raw.githubusercontent.com/Qwertys50/Cheat/refs/heads/main/tdut_origin.lua'))()
+local UI = loadstring(game:HttpGet('https://raw.githubusercontent.com/Qwertys50/gui_cheat/refs/heads/main/main.luau'))()
 local mainFrame, homeFrame, scrollFrame, pages, navFrame = UI.create_starter(screen_game)
 local imageLabel: ImageLabel, nameLabel, countryLabel = UI.create_home(game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name, homeFrame)
 
@@ -139,8 +137,6 @@ button_btn.MouseButton1Up:Connect(function()
                         local number = string.match(val, "([%d%.]+)") 
                         local boostValue = tonumber(number)
 
-                        print(boostValue, my_proz_1[i.Name], my_proz_1[i.Name] > boostValue)
-
                         if my_proz_1[i.Name] > boostValue then 
                             fireclickdetector(i.ClickDetector) 
                         else break
@@ -180,7 +176,9 @@ task.spawn(function()
 
     while task.wait() do   
         
-        if autofarms["Auto upgrade"] then
+        if autofarms["Auto upgrade"] == true then
+
+            print(1)
             local skill = workspace.Button.Skill
             local jumpify = workspace.Button.Jumpify
             local Unfailability = workspace.Button.Unfailability
@@ -208,7 +206,7 @@ task.spawn(function()
             end)
         end
 
-        if autofarms["Auto wigle"] then
+        if autofarms["Auto wigle"] == true then
             if workspace:FindFirstChild("Unlock") and workspace.Unlock:FindFirstChild("UF23") then
 
                 local clickDetector = workspace.Unlock.UF23.Wedge.ClickDetector :: ClickDetector
