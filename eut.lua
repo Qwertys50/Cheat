2c19812ef55f9536aafeb6820f29f9a07e124259
qwertys50 <kchess5gorsk2@gmail.com>
2026-03-15 21:35:02 +0300

    Update eut.lua

--- eut.lua
+++ eut.lua
@@ -14,9 +14,6 @@ local function tableLength(t)
 	return c
 end
 
-if plr.Name == "Fryziman82YT" then return end
-if plr.Name == "unknown_persona777" then return end
-
 local color_validate = Color3.new(0.717647, 1, 0.666667)
 
 makefolder("eut")
