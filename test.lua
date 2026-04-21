c988b7178290915862526dd60db3a0ea60ec7503
qwertys50 <kchess5gorsk2@gmail.com>
2026-04-21 23:23:12 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -6,4 +6,5 @@ game.Players.LocalPlayer.OnTeleport:Connect(function()
     ]], "https://raw.githubusercontent.com/Qwertys50/Cheat/refs/heads/main/test.lua"))
 end)
 
-print(1)
+task.wait(1)
+game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
