0c2465e86788bfa1a8863c296ccf8d1901f8e73e
qwertys50 <kchess5gorsk2@gmail.com>
2026-04-22 16:19:37 +0300

    Update test2.lua

--- test2.lua
+++ test2.lua
@@ -5,11 +5,23 @@ game.Players.PlayerAdded:Connect(function(player)
 
             queue_on_teleport(string.format([[
                 loadstring(game:HttpGet("%s"))()
-            ]], "https://raw.githubusercontent.com/Qwertys50/Cheat/refs/heads/main/test2.lua"))
+            ]], "https://raw.githubusercontent.com/Qwertys50/Cheat/refs/heads/main/test.lua"))
         end)
 
     end
 end)
+
+for _, i in ipairs(game.Players:GetPlayers()) do
+    
+    if i == game.Players.LocalPlayer then
+        i.OnTeleport:Connect(function()
+
+            queue_on_teleport(string.format([[
+                loadstring(game:HttpGet("%s"))()
+            ]], "https://raw.githubusercontent.com/Qwertys50/Cheat/refs/heads/main/test.lua"))
+        end)
+    end
+end
 
 task.wait(1)
 game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
