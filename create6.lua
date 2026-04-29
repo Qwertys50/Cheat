c0fc8aa9dd82d0fa6e71a85c384538c1b6256900
qwertys50 <kchess5gorsk2@gmail.com>
2026-04-30 00:33:11 +0300

    Create create6.lua

---
+++ create6.lua
@@ -0,0 +1,31 @@
+local stoped = false
+
+game.Players.PlayerAdded:Connect(function(player)
+
+    if player == game.Players.LocalPlayer then
+        
+        print(123)
+        player.OnTeleport:Connect(function()
+
+            queue_on_teleport(string.format([[
+                loadstring(game:HttpGet("%s"))()
+            ]], "https://raw.githubusercontent.com/Qwertys50/Cheat/refs/heads/main/test2.lua"))
+        end)
+
+    end
+end)
+
+for _, i in ipairs(game.Players:GetPlayers()) do
+    
+    if i == game.Players.LocalPlayer then
+    print(123)
+        i.OnTeleport:Connect(function()
+
+            queue_on_teleport(string.format([[
+                loadstring(game:HttpGet("%s"))()
+            ]], "https://raw.githubusercontent.com/Qwertys50/Cheat/refs/heads/main/test2.lua"))
+        end)
+    end
+end
+
+print(1)
