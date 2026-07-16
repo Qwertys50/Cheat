5026f7457025978e5bd13009fd17081924f6b942
qwertys50 <kchess5gorsk2@gmail.com>
2026-07-16 23:35:11 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,10 +1,1 @@
-
-game.Players.LocalPlayer.OnTeleport:Connect(function()
-
-    queue_on_teleport(string.format([[
-        loadstring(game:HttpGet("%s"))()
-    ]], "https://raw.githubusercontent.com/Qwertys50/Cheat/refs/heads/main/test.lua"))
-end)
-
-task.wait(1)
-game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
+print("FRYZI")
